cb6eecddaff95361aac49a08a48e1336538c260f
Rayfalling <wanghaiwe.i.1999@163.com>
2020-10-21 14:03:03 +0800

    fix debug mode

--- CmakeInclude/DetectPlatform.cmake
+++ CmakeInclude/DetectPlatform.cmake
@@ -6,6 +6,7 @@ if (MSVC)
     # set character set unicode to make TCHAR is alias of wchar_t
     add_definitions(-DUNICODE -D_UNICODE)
     add_compile_definitions(MSVC)
+    add_compile_options(/MP)
     if (CMAKE_CL_64)
         set(DXC_BUILD_ARCH "x64")
     else (CMAKE_CL_64)
@@ -15,6 +16,12 @@ if (MSVC)
     if (BUILD_SHARED_LIBS)
         set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS TRUE)
     endif (BUILD_SHARED_LIBS)
+
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /DEBUG")             # 调试包不优化
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /O2 /NDEBUG")    # release包优化
+elseif (MSVC)
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g")             # 调试包不优化
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O2 -DNDEBUG")    # release包优化
 endif (MSVC)
 
 # judge platform

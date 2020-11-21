26af32903da052e8ef0d9f749c7c22edfd37157e
Rayfalling <wanghaiwe.i.1999@163.com>
2020-11-21 19:04:28 +0800

    add test for render module

--- CmakeInclude/glad.cmake
+++ CmakeInclude/glad.cmake
@@ -1,13 +1,9 @@
 cmake_minimum_required(VERSION 3.14)
 
-
 # Detect 64bit python interpreter if arch is AMD64
 if (${TARGET_ARCH} STREQUAL "x64")
     option(-A x64)
 endif (${TARGET_ARCH} STREQUAL "x64")
-
-set(GLAD_API "gl=,gles=" CACHE STRING "API type/version pairs, like \"gl=3.2,gles=\", no version means latest")
-set(GLAD_SPEC "gl" CACHE STRING "Name of the spec")
 
 add_subdirectory(${CMAKE_SOURCE_DIR}/libs/glad)
 
---
+++ Test/Engine/Modules/Render/Source/main.cpp
@@ -0,0 +1,24 @@
+#include "Entry.h"
+
+
+#include <wrl.h>
+using namespace Microsoft;
+using namespace Microsoft::WRL;
+#include <dxgi1_6.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+#include <d3d12.h>
+#include <d3d12shader.h>
+#include <d3dcompiler.h>
+
+struct GRS_VERTEX {
+	XMFLOAT3 position;
+	XMFLOAT4 color;
+};
+
+int main() {
+	Engine::InitEngine();
+
+
+	return 0;
+}
